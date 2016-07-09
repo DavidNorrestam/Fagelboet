20273be64a409d353976ae86392653dfe88f9b11
david <david@DESKTOP-1CBC2F3>
2016-07-10 01:56:31 +0200

    Add project files.

---
+++ Fagelboet.Web/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace Fagelboet.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
